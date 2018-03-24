b8a7af421b77bc9ca6c20db5a0ca31729a738949
sbussetti <steve.bussetti@gmail.com>
2018-03-24 11:05:40 -0400

    retab, bump version

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,73 @@
 /* jshint node:true */
 
 var gulp = require( 'gulp' ),
-	path = require( 'path' ),
-	copy = require( 'gulp-copy' ),
-	notify = require( 'gulp-notify' ),
-	uglifycss = require( 'gulp-uglifycss' ),
-	uglify = require( 'gulp-uglify' ),
-	fs = require( 'fs' ),
-	plumber = require( 'gulp-plumber' ),
-	sourcemaps = require( 'gulp-sourcemaps' ),
-	zip = require( 'gulp-zip' ),
-	concat = require( 'gulp-concat' );
+  path = require( 'path' ),
+  copy = require( 'gulp-copy' ),
+  notify = require( 'gulp-notify' ),
+  uglifycss = require( 'gulp-uglifycss' ),
+  uglify = require( 'gulp-uglify' ),
+  fs = require( 'fs' ),
+  plumber = require( 'gulp-plumber' ),
+  sourcemaps = require( 'gulp-sourcemaps' ),
+  zip = require( 'gulp-zip' ),
+  concat = require( 'gulp-concat' );
 
 
 var manifest = require( './manifest.json' ),
-	popupJS = [ 'lib/jquery-3.0.0.min.js', 'lib/controller_communicator.js', 'lib/utils.js', 'popup.js' ],
-	optionsCSS = [ 'chrome-bootstrap.css', 'options.css' ],
-	optionsJS = [ 'lib/jquery-3.0.0.min.js', 'lib/jsrender.min.js', 'lib/utils.js', 'lib/controller_communicator.js', 'options.js' ];
+  popupJS = [ 'lib/jquery-3.0.0.min.js', 'lib/controller_communicator.js', 'lib/utils.js', 'popup.js' ],
+  optionsCSS = [ 'chrome-bootstrap.css', 'options.css' ],
+  optionsJS = [ 'lib/jquery-3.0.0.min.js', 'lib/jsrender.min.js', 'lib/utils.js', 'lib/controller_communicator.js', 'options.js' ];
 
 gulp.task( 'build-content-css', function () {
 
-	gulp.src( manifest.content_scripts[ 0 ].css )
-		.pipe( uglifycss( { uglyComments: true } ) )
-		.pipe( concat( 'content.min.css' ) )
-		.pipe( gulp.dest( './build/' ) )
-		.pipe( notify( {
-			title: 'Gulp',
-			message: 'Built style',
-			onLast: true
-		} ) );
+  gulp.src( manifest.content_scripts[ 0 ].css )
+    .pipe( uglifycss( { uglyComments: true } ) )
+    .pipe( concat( 'content.min.css' ) )
+    .pipe( gulp.dest( './build/' ) )
+    .pipe( notify( {
+      title: 'Gulp',
+      message: 'Built style',
+      onLast: true
+    } ) );
 
 } );
 
 gulp.task( 'build-options-css', function () {
 
-	gulp.src( optionsCSS )
-		.pipe( uglifycss( { uglyComments: true } ) )
-		.pipe( concat( 'options.min.css' ) )
-		.pipe( gulp.dest( './build/' ) )
-		.pipe( notify( {
-			title: 'Gulp',
-			message: 'Built options style',
-			onLast: true
-		} ) );
+  gulp.src( optionsCSS )
+    .pipe( uglifycss( { uglyComments: true } ) )
+    .pipe( concat( 'options.min.css' ) )
+    .pipe( gulp.dest( './build/' ) )
+    .pipe( notify( {
+      title: 'Gulp',
+      message: 'Built options style',
+      onLast: true
+    } ) );
 
 } );
 
 function buildJS ( src, destFile ) {
-	return function () {
+  return function () {
 
-		gulp.src( src )
-			.pipe( plumber( {
-				errorHandler: notify.onError( function ( error ) {
-					return error.name + ': ' + error.message + '\n' + error.cause.filename + '[' + error.cause.line + ':' + error.cause.col + '] ' + error.cause.message;
-				} )
-			} ) )
-			.pipe( sourcemaps.init() )
-			.pipe( uglify() )
-			.pipe( plumber.stop() )
-			.pipe( concat( destFile ) )
-			.pipe( sourcemaps.write( 'maps' ) )
-			.pipe( gulp.dest( './build/' ) )
-			.pipe( notify( {
-				title: 'Gulp',
-				message: 'Built: ' + destFile,
-				onLast: true
-			} ) );
+    gulp.src( src )
+      .pipe( plumber( {
+        errorHandler: notify.onError( function ( error ) {
+          return error.name + ': ' + error.message + '\n' + error.cause.filename + '[' + error.cause.line + ':' + error.cause.col + '] ' + error.cause.message;
+        } )
+      } ) )
+      .pipe( sourcemaps.init() )
+      .pipe( uglify() )
+      .pipe( plumber.stop() )
+      .pipe( concat( destFile ) )
+      .pipe( sourcemaps.write( 'maps' ) )
+      .pipe( gulp.dest( './build/' ) )
+      .pipe( notify( {
+        title: 'Gulp',
+        message: 'Built: ' + destFile,
+        onLast: true
+      } ) );
 
-	};
+  };
 }
 
 gulp.task( 'build-content-js', buildJS( manifest.content_scripts[ 0 ].js, 'content.min.js' ) );
@@ -79,55 +79,55 @@ gulp.task( 'build-popup-js', buildJS( popupJS, 'popup.min.js' ) );
 gulp.task( 'build-options-js', buildJS( optionsJS, 'options.min.js' ) );
 
 gulp.task( 'copy-project-files', function ( callback ) {
-	delete require.cache[ path.resolve( './manifest.json' ) ];
-	var manifest = require( './manifest.json' );
-	manifest.content_scripts[ 0 ].css = [ 'content.min.css' ];
-	manifest.content_scripts[ 0 ].js = [ 'content.min.js' ];
-	manifest.background.scripts = [ 'background.min.js' ];
+  delete require.cache[ path.resolve( './manifest.json' ) ];
+  var manifest = require( './manifest.json' );
+  manifest.content_scripts[ 0 ].css = [ 'content.min.css' ];
+  manifest.content_scripts[ 0 ].js = [ 'content.min.js' ];
+  manifest.background.scripts = [ 'background.min.js' ];
 
-	if ( !fs.existsSync( './build' ) ) {
-		fs.mkdirSync( './build' );
-	}
-	fs.writeFile( './build/manifest.json', JSON.stringify( manifest ), callback );
+  if ( !fs.existsSync( './build' ) ) {
+    fs.mkdirSync( './build' );
+  }
+  fs.writeFile( './build/manifest.json', JSON.stringify( manifest ), callback );
 
-	gulp.src( [
-		'README.md',
-		'./images/*',
-		'options.html',
-		'popup.html'
-	] )
-		.pipe( copy( './build/' ) )
-		.pipe( gulp.dest( './' ) )
-		.pipe( notify( {
-			title: 'Gulp',
-			message: 'Copied project files',
-			onLast: true
-		} ) );
+  gulp.src( [
+    'README.md',
+    './images/*',
+    'options.html',
+    'popup.html'
+  ] )
+    .pipe( copy( './build/' ) )
+    .pipe( gulp.dest( './' ) )
+    .pipe( notify( {
+      title: 'Gulp',
+      message: 'Copied project files',
+      onLast: true
+    } ) );
 } );
 
 gulp.task( 'package', function () {
 
-	var buildManifest = require( './build/manifest.json' );
+  var buildManifest = require( './build/manifest.json' );
 
-	return gulp.src( 'build/**/*' )
-		.pipe( zip( 'deluge-siphon-' + buildManifest.version + '.zip' ) )
-		.pipe( gulp.dest( 'dist' ) )
-		.pipe( notify( {
-			title: 'Gulp',
-			message: 'Packaged...',
-			onLast: true
-		} ) );
+  return gulp.src( 'build/**/*' )
+    .pipe( zip( 'deluge-siphon-' + buildManifest.version + '.zip' ) )
+    .pipe( gulp.dest( 'dist' ) )
+    .pipe( notify( {
+      title: 'Gulp',
+      message: 'Packaged...',
+      onLast: true
+    } ) );
 } );
 
 function watch () {
 
-	gulp.watch( manifest.content_scripts[ 0 ].css, [ 'build-content-css' ] );
-	gulp.watch( manifest.content_scripts[ 0 ].js, [ 'build-content-js' ] );
-	gulp.watch( manifest.background.scripts, [ 'build-background-js' ] );
-	gulp.watch( popupJS, [ 'build-popup-js' ] );
-	gulp.watch( optionsJS, [ 'build-options-js' ] );
-	gulp.watch( optionsCSS, [ 'build-options-css' ] );
-	gulp.watch( [ '*.json', '*.html' ], [ 'copy-project-files' ] );
+  gulp.watch( manifest.content_scripts[ 0 ].css, [ 'build-content-css' ] );
+  gulp.watch( manifest.content_scripts[ 0 ].js, [ 'build-content-js' ] );
+  gulp.watch( manifest.background.scripts, [ 'build-background-js' ] );
+  gulp.watch( popupJS, [ 'build-popup-js' ] );
+  gulp.watch( optionsJS, [ 'build-options-js' ] );
+  gulp.watch( optionsCSS, [ 'build-options-css' ] );
+  gulp.watch( [ '*.json', '*.html' ], [ 'copy-project-files' ] );
 
 }
 
